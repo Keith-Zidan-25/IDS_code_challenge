d60c2661f370c5b6be6fd00d73f645039190c01b
Keith-Zidan-25 <keithzidand@gmail.com>
2025-11-16 20:23:27 +0530

    turbopack-config

--- hcs-messaging-app/next.config.ts
+++ hcs-messaging-app/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    resolveExtensions: [".md", ".js", ".svg", ".zip", ".yml", ".mjs"]
+  }
 };
 
 export default nextConfig;
